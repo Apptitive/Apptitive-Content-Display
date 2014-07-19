e239c16d446fcccf42e672898585f28f698c8908
androidcodehunter <sharif.iit.du@gmail.com>
2014-07-19 14:31:07 +0600

    added db add, delete, update logic

--- content display/src/main/java/com/apptitive/content_display/sync/SyncAdapter.java
+++ content display/src/main/java/com/apptitive/content_display/sync/SyncAdapter.java
@@ -14,21 +14,18 @@ import com.apptitive.content_display.model.DbContent;
 import com.apptitive.content_display.utilities.Config;
 import com.apptitive.content_display.utilities.Constants;
 import com.apptitive.content_display.utilities.HttpHelper;
+import com.apptitive.content_display.utilities.JsonParser;
 import com.apptitive.content_display.utilities.LogUtil;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by Sharif on 7/16/2014.
  */
 public class SyncAdapter extends AbstractThreadedSyncAdapter implements JsonArrayCompleteListener<JSONArray> {
+
+    private DbManager dbManager;
 
     public SyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
@@ -38,6 +35,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter implements JsonArra
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
         LogUtil.LOGE("inside Syncadapter");
         DbManager.init(getContext());
+        dbManager = DbManager.getInstance();
         HttpHelper httpHelper = HttpHelper.getInstance(getContext(), this);
         httpHelper.getJsonArray(Config.getMenuUrl(), Constants.MENU_REQUEST_CODE);
         httpHelper.getJsonArray(Config.getTopicUrl(), Constants.CONTENT_REQUEST_CODE);
@@ -51,36 +49,13 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter implements JsonArra
 
     @Override
     public void onJsonArray(JSONArray result, int requestCode) {
-        Gson gson = new Gson();
         if (requestCode == Constants.MENU_REQUEST_CODE) {
-            List<ContentMenu> menus = Arrays.asList(gson.fromJson(result.toString(), ContentMenu[].class));
-            DbManager.getInstance().addMenu(menus);
+            ContentMenu.updateDb(dbManager, JsonParser.getParsedContentMenus(result.toString()));
         } else if (requestCode == Constants.CONTENT_REQUEST_CODE) {
-            DbManager.getInstance().addDbContent(getParsedDbContentResult(result));
+            DbContent.updateDb(dbManager, JsonParser.getParsedDbContents(result));
             LogUtil.LOGE("successful");
         }
     }
 
-    private List<DbContent> getParsedDbContentResult(JSONArray result) {
-        List<DbContent> dbContents = new ArrayList<DbContent>();
-        for (int i = 0; i < result.length(); i++) {
-            try {
-                JSONObject jsonObject = (JSONObject) result.get(i);
-                DbContent dbContent = new DbContent();
-                dbContent.setActionId((Integer) jsonObject.get("actionId"));
-                dbContent.setContentId(jsonObject.get("contentId").toString());
-                dbContent.setMenuId(jsonObject.get("menuId").toString());
-                dbContent.setHeader(jsonObject.get("header").toString());
-                dbContent.setShortDescription(jsonObject.get("shortDescription").toString());
-                dbContent.setDetails(jsonObject.get("details").toString());
-                dbContent.setViewType(jsonObject.get("viewType").toString());
-                dbContent.setActionType(jsonObject.get("actionType").toString());
-                dbContents.add(dbContent);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
 
-        return dbContents;
-    }
 }
