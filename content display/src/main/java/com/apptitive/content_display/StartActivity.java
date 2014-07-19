4f55862145d654622ad1b99f931edb21feb433b8
rayhan <hossain.rayhan@outlook.com>
2014-07-19 12:00:38 +0600

    Adapter for ContentMenuList is prepared. (continue)

--- content display/src/main/java/com/apptitive/content_display/StartActivity.java
+++ content display/src/main/java/com/apptitive/content_display/StartActivity.java
@@ -1,16 +1,22 @@
 package com.apptitive.content_display;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewStub;
-import android.widget.TextView;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.apptitive.content_display.adapter.ContentMenuAdapter;
 import com.apptitive.content_display.helper.DbManager;
-import com.apptitive.content_display.model.Menu;
+import com.apptitive.content_display.model.ContentMenu;
+import com.apptitive.content_display.sync.SyncUtils;
 
 import java.util.List;
 
 public class StartActivity extends ActionBarActivity {
+
+    private ContentMenuAdapter contentMenuAdapter;
+    private List<ContentMenu> contentMenuList;
+    private ListView lvContentMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +24,31 @@ public class StartActivity extends ActionBarActivity {
         setContentView(R.layout.activity_start);
         DbManager.init(this);
 
-        List<Menu> menuList = DbManager.getInstance().getAllMenus();
 
-        ViewStub stubActivityStart = (ViewStub)findViewById(R.id.view_stub_activity_start);
+        ContentMenu contentMenu1 = new ContentMenu(100,"100","Ramjan","add",1,2);
+        ContentMenu contentMenu2 = new ContentMenu(200,"200","Sob e Kadar","add",1,2);
+        ContentMenu contentMenu3 = new ContentMenu(300,"300","Tarabih","add",1,2);
+        ContentMenu contentMenu4 = new ContentMenu(400,"400","Niot Dua","add",1,2);
+        ContentMenu contentMenu5 = new ContentMenu(500,"500","Eid","add",1,2);
+        ContentMenu contentMenu6 = new ContentMenu(600,"600","Salat","add",1,2);
+        DbManager.getInstance().addMenu(contentMenu1);
+        DbManager.getInstance().addMenu(contentMenu2);
+        DbManager.getInstance().addMenu(contentMenu3);
+        DbManager.getInstance().addMenu(contentMenu4);
+        DbManager.getInstance().addMenu(contentMenu5);
+        DbManager.getInstance().addMenu(contentMenu6);
+        SyncUtils.triggerManualSync();
+        lvContentMenu = (ListView) findViewById(R.id.lv_content_menu);
+        contentMenuList = DbManager.getInstance().getAllMenus();
+        contentMenuAdapter = new ContentMenuAdapter(this, R.layout.list_item_content_menu, contentMenuList);
+        lvContentMenu.setAdapter(contentMenuAdapter);
+
+        Log.e("Call","Called");
+        for (ContentMenu m : contentMenuList){
+            Log.e("Menu Title ",m.getTitle());
+        }
+
+/*        ViewStub stubActivityStart = (ViewStub)findViewById(R.id.view_stub_activity_start);
         stubActivityStart.setLayoutResource(R.layout.menu_pattern_1);
         View activityStart = stubActivityStart.inflate();
 
@@ -36,7 +64,7 @@ public class StartActivity extends ActionBarActivity {
 
         ViewStub stubRight = (ViewStub)activityStart.findViewById(R.id.sub_pattern_right);
         stubRight.setLayoutResource(R.layout.partial_view_top_to_bottom);
-        stubRight.inflate();
+        stubRight.inflate();*/
     }
 
 }
