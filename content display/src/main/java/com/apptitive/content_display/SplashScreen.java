bcefecb0d7021ef40aebb2dee66f672cc4220199
rayhan <hossain.rayhan@outlook.com>
2014-07-24 15:29:28 +0600

    MainActivity.java is Ready . clone of StartActivity.java. Unnecessary files are dropped.

--- content display/src/main/java/com/apptitive/content_display/SplashScreen.java
+++ content display/src/main/java/com/apptitive/content_display/SplashScreen.java
@@ -4,9 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-
-import com.apptitive.content_display.sync.SyncUtils;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -16,14 +13,11 @@ public class SplashScreen extends Activity {
     @Override
     public void onStart() {
         super.onStart();
-       // SyncUtils.triggerManualSync();
-        // EasyTracker.getInstance(this).activityStart(this);
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        // EasyTracker.getInstance(this).activityStop(this);
     }
 
     @Override
@@ -31,14 +25,11 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_splash_screen);
-        SyncUtils.createSyncAccount(this);
-        SyncUtils.triggerManualSync();
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-
-                startActivity(new Intent(SplashScreen.this, StartActivity.class));
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
                 finish();
             }
         }, 2000);
--- content display/src/main/java/com/apptitive/content_display/StartActivity.java
+++ content display/src/main/java/com/apptitive/content_display/StartActivity.java
@@ -13,23 +13,18 @@ import android.view.ViewGroup;
 import android.view.ViewStub;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-
 import com.android.volley.toolbox.ImageLoader;
-
 import com.android.volley.toolbox.NetworkImageView;
 import com.apptitive.content_display.helper.DbManager;
 import com.apptitive.content_display.helper.DisplayPattern;
 import com.apptitive.content_display.helper.Helper;
 import com.apptitive.content_display.model.ContentMenu;
 import com.apptitive.content_display.services.SyncService;
-import com.apptitive.content_display.utilities.AlarmUtil;
 import com.apptitive.content_display.utilities.Config;
 import com.apptitive.content_display.utilities.Constants;
 import com.apptitive.content_display.utilities.HttpHelper;
 import com.apptitive.content_display.utilities.LogUtil;
 import com.apptitive.content_display.utilities.PreferenceHelper;
-
 import java.util.List;
 
 public class StartActivity extends ActionBarActivity {
@@ -64,33 +59,8 @@ public class StartActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         DbManager.init(this);
-
-        AlarmUtil.setUpAlarm(this,1);
-        //  ringProgressDialog = ProgressDialog.show(StartActivity.this, "Please wait ...", "Downloading Image ...", true);
-        // ringProgressDialog.setCancelable(true);
-
-      /*ContentMenu contentMenu1 = new ContentMenu(1, "1", "Title 1", "add", 1, 1);
-        ContentMenu contentMenu2 = new ContentMenu(2, "2", "Title 2", "add", 1, 2);
-        ContentMenu contentMenu3 = new ContentMenu(3, "3", "Title 3", "add", 1, 3);
-
-        ContentMenu contentMenu4 = new ContentMenu(4, "4", "Title 4", "add", 2, 1);
-        ContentMenu contentMenu5 = new ContentMenu(5, "5", "Title 5", "add", 2, 2);
-        ContentMenu contentMenu6 = new ContentMenu(6, "6", "Title 6", "add", 2, 3);
-
-        ContentMenu contentMenu7 = new ContentMenu(7, "7", "Title 7", "add", 3, 1);
-
-        DbManager.getInstance().addMenu(contentMenu1);
-        DbManager.getInstance().addMenu(contentMenu2);
-        DbManager.getInstance().addMenu(contentMenu3);
-
-        DbManager.getInstance().addMenu(contentMenu7);
-
-        DbManager.getInstance().addMenu(contentMenu4);
-        DbManager.getInstance().addMenu(contentMenu5);
-        DbManager.getInstance().addMenu(contentMenu6);*/
         if (isContentShowInOncreate)
             renderContentMenu();
-
     }
 
 
@@ -107,10 +77,6 @@ public class StartActivity extends ActionBarActivity {
         llMain = (LinearLayout) findViewById(R.id.ll_main);
         contentMenuList = DbManager.getInstance().getAllMenus();
 
-        for (ContentMenu c : contentMenuList) {
-            Log.e("Menu Title ", c.getTitle());
-        }
-
         for (currentMenu = 0; currentMenu < contentMenuList.size(); ) {
             int patternId = contentMenuList.get(currentMenu).getPatternId();
 
@@ -118,6 +84,7 @@ public class StartActivity extends ActionBarActivity {
                 View view = getViewForContentMenuPattern(R.layout.menu_pattern_1);
                 populateContentMenuItem(view, R.id.sub_pattern_left_top, contentMenuList.get(currentMenu++), DisplayPattern.LeftToRight);
                 populateContentMenuItem(view, R.id.sub_pattern_left_bottom, contentMenuList.get(currentMenu++), DisplayPattern.LeftToRight);
+                populateContentMenuItem(view, R.id.sub_pattern_right, contentMenuList.get(currentMenu++), DisplayPattern.TopToBottom);
             } else if (patternId == 2) {
                 View view = getViewForContentMenuPattern(R.layout.menu_pattern_2);
                 populateContentMenuItem(view, R.id.sub_pattern_left, contentMenuList.get(currentMenu++), DisplayPattern.TopToBottom);
@@ -125,10 +92,10 @@ public class StartActivity extends ActionBarActivity {
                 populateContentMenuItem(view, R.id.sub_pattern_right_bottom, contentMenuList.get(currentMenu++), DisplayPattern.LeftToRight);
             } else if (patternId == 3) {
                 View view = getViewForContentMenuPattern(R.layout.menu_pattern_3);
+                populateContentMenuItem(view, R.id.sub_pattern_whole, contentMenuList.get(currentMenu++), DisplayPattern.Fill);
             }
         }
     }
-
 
     private View getViewForContentMenuPattern(int layoutId) {
 
@@ -150,9 +117,8 @@ public class StartActivity extends ActionBarActivity {
                 stub.setLayoutResource(R.layout.partial_view_left_right);
             else
                 stub.setLayoutResource(R.layout.partial_view_top_to_bottom);
-        } else if (displayPattern.equals(DisplayPattern.ToptoBottom)) {
+        } else if (displayPattern.equals(DisplayPattern.TopToBottom)) {
             stub.setLayoutResource(R.layout.partial_view_top_to_bottom);
-        } else if (displayPattern.equals(DisplayPattern.Whole)) {
         } else if (displayPattern.equals(DisplayPattern.Fill)) {
             stub.setLayoutResource(R.layout.partial_view_whole);
         }
